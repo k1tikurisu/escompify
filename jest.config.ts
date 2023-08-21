ae41175e248e8a444a7f249bcb9521bf49214512
Daiki Maekawa <daaikii@outlook.jp>
2023-08-21 23:57:40 +0900

    chore: Update build and test configuration, and ignore test resources in linting

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,10 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*+(spec|test).+(ts|tsx|js)']
+  testMatch: ['**/*+(spec|test).+(ts|tsx|js)'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  }
 };
 
 export default config;
