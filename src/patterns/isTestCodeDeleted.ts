619766d4c717ff1a8338f1017f664d41c768c76b
Daiki Maekawa <daaikii@outlook.jp>
2023-08-23 00:32:55 +0900

    fix: Support nested test cases in the 'isTestCodeDeleted' function

--- src/patterns/isTestCodeDeleted.ts
+++ src/patterns/isTestCodeDeleted.ts
@@ -1,11 +1,23 @@
 import { ActionType } from '@/services/gumtree';
 import { isTestCode } from '@/utils/patterns';
+import { simple } from 'acorn-walk';
+import { Node } from 'estree';
+import acorn from 'acorn';
 
 export function isTestCodeDeleted(action: ActionType) {
-  if (action.action === 'delete-tree') {
-    if (action.src.node.type === 'ExpressionStatement') {
-      return isTestCode(action.src.node.expression);
+  try {
+    if (action.action === 'delete-tree') {
+      simple(action.src.node as acorn.Node, {
+        CallExpression(node) {
+          if (isTestCode(node as Node)) {
+            // stop the walk (https://github.com/acornjs/acorn/issues/625)
+            throw true;
+          }
+        }
+      });
     }
+  } catch {
+    return true;
   }
 
   return false;
