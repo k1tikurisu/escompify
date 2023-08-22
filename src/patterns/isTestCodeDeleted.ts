f013fa3d58e0e3ddcc3f342bd1c97ab1fec1c1fe
Daiki Maekawa <daaikii@outlook.jp>
2023-08-23 02:11:36 +0900

    fix: Fix type missmatch between estree and acorn

--- src/patterns/isTestCodeDeleted.ts
+++ src/patterns/isTestCodeDeleted.ts
@@ -1,15 +1,13 @@
 import { ActionType } from '@/services/gumtree';
 import { isTestCode } from '@/utils/patterns';
 import { simple } from 'acorn-walk';
-import { Node } from 'estree';
-import acorn from 'acorn';
 
 export function isTestCodeDeleted(action: ActionType) {
   try {
     if (action.action === 'delete-tree') {
-      simple(action.src.node as acorn.Node, {
+      simple(action.src.node, {
         CallExpression(node) {
-          if (isTestCode(node as Node)) {
+          if (isTestCode(node)) {
             // stop the walk (https://github.com/acornjs/acorn/issues/625)
             throw true;
           }
