26817ad7958e472eb351ae068bac89f6f82f120a
Daiki Maekawa <daaikii@outlook.jp>
2023-08-31 10:56:45 +0900

    fix: Fix the issue to ensure a block is inserted when merging files

--- src/utils/files/mergeFileIfExists.ts
+++ src/utils/files/mergeFileIfExists.ts
@@ -3,7 +3,7 @@ import { existsSync, readFileSync } from 'fs';
 export function mergeFileIfExists(filePaths: string[]): string {
   const fileContents = filePaths
     .filter((filePath) => existsSync(filePath))
-    .map((filePath) => readFileSync(filePath, 'utf8'));
+    .map((filePath) => `{${readFileSync(filePath, 'utf8')}}`);
 
   const mergedContents = fileContents.join('\n');
 
