382b24e24fcad6f7357d90881acc86b77aaa3ada
Daiki Maekawa <daaikii@outlook.jp>
2023-09-29 15:31:38 +0900

    feat: Create a GumTree runner

---
+++ src/gumtree/runGumTree.ts
@@ -0,0 +1,56 @@
+import path from 'path';
+import os from 'os';
+import fs from 'fs';
+import { randomUUID } from 'crypto';
+import { run } from '@/utils/run';
+
+export type GumTreeResponseType = {
+  matches: {
+    src: string;
+    dest: string;
+  }[];
+  actions: {
+    action: ActionType;
+    tree: string;
+    parent?: string;
+    at?: number;
+    label?: string;
+  }[];
+};
+
+type ActionType =
+  | 'match'
+  | `insert-${'node' | 'tree'}`
+  | `move-${'node' | 'tree'}`
+  | `update-${'node' | 'tree'}`
+  | `delete-${'node' | 'tree'}`;
+
+export async function runGumTree(
+  srcCode: string,
+  dstCode: string
+): Promise<GumTreeResponseType | undefined> {
+  const id = randomUUID();
+  const tempDir = path.join(os.tmpdir(), id);
+
+  try {
+    fs.mkdirSync(tempDir);
+
+    const srcPath = path.join(tempDir, `src.js`);
+    const dstPath = path.join(tempDir, `dst.js`);
+    fs.writeFileSync(srcPath, srcCode, 'utf-8');
+    fs.writeFileSync(dstPath, dstCode, 'utf-8');
+
+    const output = await run(
+      `gumtree textdiff -x /usr/bin/tsparser -f JSON ${srcPath} ${dstPath}`
+    );
+    console.log(output);
+
+    return JSON.parse(output);
+  } catch (error) {
+    console.error('Error at generateAction: ', error);
+  } finally {
+    if (fs.existsSync(tempDir)) {
+      fs.rmSync(tempDir, { recursive: true, force: true });
+    }
+  }
+}
