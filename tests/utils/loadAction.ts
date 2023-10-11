86174711c0ae60e403bc14342493cde0682f1bea
Daiki Maekawa <daaikii@outlook.jp>
2023-10-11 15:25:04 +0900

    fix: Delete console.log

--- tests/utils/loadAction.ts
+++ tests/utils/loadAction.ts
@@ -2,7 +2,6 @@ import { generateActions } from '../../src/gumtree';
 
 export async function loadAction(srcCode: string, dstCode: string) {
   const output = await generateActions(srcCode, dstCode);
-  console.log(output);
 
   return output ?? [];
 }
