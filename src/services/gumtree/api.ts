d1f16b54fb148ca238c4b6d6ff78acc2d1e025f8
Daiki Maekawa <daaikii@outlook.jp>
2023-10-11 15:05:49 +0900

    chore: Remove api-related codes

--- src/services/gumtree/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'http://localhost:8000',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-});
--- src/services/gumtree/index.ts
+++
@@ -1,7 +0,0 @@
-// entrypoint
-export { api } from '@/services/gumtree/api';
-
-// endpoints
-export { postDiff } from '@/services/gumtree/post/post';
-export { getAction, type ActionType } from '@/services/gumtree/action/action';
-export { getAst } from '@/services/gumtree/ast/ast';
